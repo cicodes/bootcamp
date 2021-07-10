3583f90b7ad46f2e2d8016ffa32e528003b216d4
cicodes <35441543+cicodes@users.noreply.github.com>
2021-07-11 02:11:15 +0300

    Update Odev2.sql

--- Odev2.sql
+++ Odev2.sql
@@ -1,6 +1,6 @@
 --Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 
---film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla
+--1. film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla
 --sıralayınız ( BETWEEN - AND yapısını kullanınız.)
 SELECT * FROM film
 WHERE replacement_cost BETWEEN 12.99 AND 16.99;
